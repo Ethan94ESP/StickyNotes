7974fa0073d4e2ed2afaf34e216452c66d1bb4e1
Ethan94ESP <ethancrespopueyo@gmail.com>
2023-01-02 19:53:42 +0100

    Unit Test example

---
+++ app/src/androidTest/java/com/ethan/stickynotes/di/TestAppModule.kt
@@ -0,0 +1,46 @@
+package com.ethan.stickynotes.di
+
+import android.app.Application
+import androidx.room.Room
+import com.ethan.stickynotes.data.NoteDatabase
+import com.ethan.stickynotes.data.repository.NoteRepositoryImpl
+import com.ethan.stickynotes.domain.repository.NoteRepository
+import com.ethan.stickynotes.domain.use_case.*
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TestAppModule {
+
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(app: Application): NoteDatabase {
+        return Room.inMemoryDatabaseBuilder(
+            app,
+            NoteDatabase::class.java,
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(db: NoteDatabase): NoteRepository {
+        return NoteRepositoryImpl(db.noteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteUseCases(repository: NoteRepository): NoteUseCases {
+        return NoteUseCases(
+            getNotes = GetNotes(repository),
+            deleteNote = DeleteNote(repository),
+            addNote = AddNote(repository),
+            getNote = GetNote(repository)
+        )
+    }
+
+
+}
